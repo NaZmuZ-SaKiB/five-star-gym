154fb329486a7d9bd7d3ca4343606a8c815be5ce
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-10-19 23:48:13 +0600

    commit init

--- src/Components/Trainers/Trainer.js
+++ src/Components/Trainers/Trainer.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import './Trainer.css';
+
 const Trainer = () => {
     const [trainers, setTrainers] = useState([]);
 
@@ -16,8 +18,15 @@ const Trainer = () => {
             <h1>Your Trainers</h1>
             {
                 trainers.map(trainer => (
-                    <div className="trainer">
-                        <img src={trainer.photo} alt="" />
+                    <div key={trainer.id} className="trainer">
+                        <div className="photo">
+                            <img src={trainer.photo} alt="trainer" />
+                        </div>
+                        <div className="info">
+                            <p className="name">{trainer.name}</p>
+                            <p className="students">Students taught :{trainer.students}</p>
+                            <button className="follow">Follow <i className="fab fa-facebook"></i></button>
+                        </div>
                     </div>
                 ))
             }
